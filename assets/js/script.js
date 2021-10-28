3194d265b1d1aa5d5ca5045201585254dc09e526
Daniel Grazziotti <dgplays14@gmail.com>
2021-10-27 21:10:18 -0300

    added: saving tasks in localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ const todosContainer = document.querySelector('.todo-list')
 const todos = todosContainer.childNodes
 const clearButton = document.querySelector('.controllers__clear-completed__button')
 const filterButtons =  document.querySelectorAll('.controllers__filter__button')
+const localStorageTasks = JSON.parse(localStorage.getItem('tasks'))
+const tasks = localStorage.getItem('tasks') !== null ? localStorageTasks : [] 
 const elementsWithAnimate = document.querySelectorAll('[data-anime]')
 
 setTimeout( () => {
@@ -12,7 +14,9 @@ setTimeout( () => {
 })
 
 // Toggle light and dark mode
-const toggleTheme = () => html.classList.toggle('dark-mode')
+function toggleTheme() {
+    html.classList.toggle('dark-mode')  
+}
 toggleButton.addEventListener('click', toggleTheme)
 
 // Add new todos to the list
@@ -20,8 +24,10 @@ function newTodo() {
     const todo = document.createElement('div')
     const todoText = input.value
 
-    todo.setAttribute('draggable', 'true')
     todo.classList.add('todo-list__todo')
+    todo.classList.add('active')
+    todo.setAttribute('draggable', 'true')
+    todo.setAttribute('data-index', todos.length)
 
     const todoContent = `
         <div class="todo-list__todo__content">
@@ -36,21 +42,88 @@ function newTodo() {
     addTodoEventListeners(todo)
     
     todosContainer.appendChild(todo)
+    
+    saveTodo(todo)
 }
+
+function saveTodo(todo) {
+    const task = todo.querySelector('.todo-list__todo__text').textContent 
+    const status = todo.classList.contains('active') 
+        ? 'active'
+        : 'completed'
+    tasks.push({status, task})
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function reorderTodos() {
+    tasks.splice(0, tasks.length)
+
+    todos.forEach( (todo, index) => {
+        todo.setAttribute('data-index', index)
+        saveTodo(todo)
+    })
+
+    console.log(tasks)
+}
+
+function renderTodos() {
+    tasks.forEach( task => {
+        const todo = document.createElement('div')
+        const todoText = task.task
+
+        todo.classList.add('todo-list__todo')
+        todo.classList.add(task.status)
+        todo.setAttribute('draggable', 'true')
+        todo.setAttribute('data-index', todos.length)
+
+        const todoContent = `
+            <div class="todo-list__todo__content">
+                <div class="checkbox"></div>
+                <div class="todo-list__todo__text">${todoText}</div>
+            </div>
+            <div class="todo-list__todo__delete"></div>  
+        `
+
+        todo.innerHTML = todoContent
+
+        addTodoEventListeners(todo)
+
+        todosContainer.appendChild(todo)
+    
+        updateTodosLeft()
+    })
+}
+
 function addTodoEventListeners(todo) {
     // Mark todos as complete
     const todoCheckbox = todo.querySelector('.checkbox')
     todoCheckbox.addEventListener('click', () => {
-        todoCheckbox.closest('.todo-list__todo').classList.toggle('is-completed')
+        todo.classList.toggle('active')
+        todo.classList.toggle('completed')
         updateTodosLeft()
-        const filterActive = document.querySelector('.controllers__filter__button.is-active')
+
+        const todoIndex = todo.getAttribute('data-index')
+        
+        tasks[todoIndex].status === 'active'
+            ? tasks[todoIndex].status = 'completed'
+            : tasks[todoIndex].status = 'active'
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+        
+        const filterActive = document.querySelector('.controllers__filter__button.active')
         filterActive.click()
     })
 
     // Delete todos from the list
     const todoDeleteBtn = todo.querySelector('.todo-list__todo__delete')
     todoDeleteBtn.addEventListener('click', () => {
+        const todoIndex = todo.getAttribute('data-index')
+        
         todosContainer.removeChild(todo)
+        tasks.splice(todoIndex, 1)
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+        
+        reorderTodos()
+        updateTodosLeft()
     })
 
     // Drag and drop to reorder items on the list
@@ -70,7 +143,11 @@ function addTodoEventListeners(todo) {
             todosContainer.insertBefore(draggable, afterElement)
         }
     })
+    todo.addEventListener('drop', () => {
+        reorderTodos()
+    })
 }
+
 function getDragAfterElement(y) {
     const draggablesTodos = [...todosContainer.querySelectorAll('.todo-list__todo:not(.dragging)')]
     return draggablesTodos.reduce((closest, child) => {
@@ -84,19 +161,22 @@ function getDragAfterElement(y) {
     }, {offset: Number.NEGATIVE_INFINITY}).element
 
 }
+
 function updateTodosLeft() {
     const todosLeftContainer = document.querySelector('.controllers__items-left__number')
     let todosLeft = 0
 
     todos.forEach( todo => {
-        if (!todo.classList.contains('is-completed')) todosLeft++
+        if (!todo.classList.contains('completed')) todosLeft++
     })
 
     todosLeftContainer.innerHTML = todosLeft
 }
+
 function clearInputText() {
     input.value = ''
 }
+
 function checkInput(evt) {
     if (evt.keyCode === 13 && input.value !== '') {
         newTodo()
@@ -104,13 +184,14 @@ function checkInput(evt) {
         clearInputText()
     }
 }
+
 input.addEventListener('keydown', checkInput)
 
 // Filter by all/active/complete todos
 function filter(evt) {
-    filterButtons.forEach( btn => btn.classList.remove('is-active'))
+    filterButtons.forEach( btn => btn.classList.remove('active'))
     const filterButton = evt.target
-    filterButton.classList.add('is-active')
+    filterButton.classList.add('active')
     
     const filter = evt.target.getAttribute('data-option')
 
@@ -120,7 +201,7 @@ function filter(evt) {
             break
         case 'completed':
             todos.forEach( todo => {
-                todo.classList.contains('is-completed')
+                todo.classList.contains('completed')
                     ?   todo.classList.remove('is-hidden')
                     :   todo.classList.add('is-hidden')
                 
@@ -128,7 +209,7 @@ function filter(evt) {
             break
         case 'active':
             todos.forEach( todo => {
-                !todo.classList.contains('is-completed')
+                !todo.classList.contains('completed')
                     ?   todo.classList.remove('is-hidden')
                     :   todo.classList.add('is-hidden')
             })
@@ -141,9 +222,18 @@ filterButtons.forEach( filterButton => {
 
 // Clear all completed todos
 function clearCompleted() {
-    const todosCompleted = todosContainer.querySelectorAll('.is-completed')
+    const todosCompleted = todosContainer.querySelectorAll('.completed')
     todosCompleted.forEach( todo => {
         todosContainer.removeChild(todo)
     })
+
+    tasks.forEach( (task, index) => {
+        if (task.status === 'completed') tasks.splice(index, 1)
+    })
+
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    
 }
 clearButton.addEventListener('click', clearCompleted)
+
+renderTodos()
